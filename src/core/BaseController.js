2e6dfa61f9bf721c2edf472c27a45523e0be6499
Berkay Aytekin <berkay.0567@gmail.com>
2025-06-03 04:13:01 +0300

    logger info-error

--- src/core/BaseController.js
+++ src/core/BaseController.js
@@ -1,6 +1,7 @@
 // src/core/BaseController.js
 import { HTTP_CODES } from "../config/Enum.js";
 import ApiError from "../utils/ApiError.js";
+import logger from "../utils/logger.js";
 
 export default class BaseController {
   constructor(service) {
@@ -16,8 +17,10 @@ export default class BaseController {
   async create(req, res, next) {
     try {
       const data = await this.service.create(req.body, req.user?._id);
+      logger.info(`Created record by user  ${req.user?._id}`);
       res.status(HTTP_CODES.CREATED).json(data);
     } catch (err) {
+      logger.error(`Create failed: ${err.message}`);
       next(err);
     }
   }
@@ -25,8 +28,10 @@ export default class BaseController {
   async getAll(req, res, next) {
     try {
       const data = await this.service.getAll(req.query);
+      logger.info(`Fetched list - query: ${JSON.stringify(req.query)}`);
       res.status(HTTP_CODES.OK).json(data);
     } catch (err) {
+      logger.error(`[${req.method}] ${req.originalUrl} → ${err.message}`);
       next(err);
     }
   }
@@ -37,20 +42,28 @@ export default class BaseController {
       if (!data) {
         throw new ApiError(HTTP_CODES.NOT_FOUND, "No record found!");
       }
+      logger.info(`Fetched record by ID ${req.params.id}`);
       res.status(HTTP_CODES.OK).json(data);
     } catch (err) {
+      logger.error(`Fetch by ID failed: ${err.message}`);
       next(err);
     }
   }
 
   async update(req, res, next) {
     try {
-      const data = await this.service.update(req.params.id, req.body, req.user?._id);
+      const data = await this.service.update(
+        req.params.id,
+        req.body,
+        req.user?._id
+      );
       if (!data) {
         throw new ApiError(HTTP_CODES.NOT_FOUND, "No record found to update!");
       }
+      logger.info(`Updated record ${req.params.id} by user ${req.user?._id}`);
       res.status(HTTP_CODES.OK).json(data);
     } catch (err) {
+      logger.error(`Update failed: ${err.message}`);
       next(err);
     }
   }
@@ -61,8 +74,10 @@ export default class BaseController {
       if (!data) {
         throw new ApiError(HTTP_CODES.NOT_FOUND, "No record found to delete!");
       }
+      logger.info(`Deleted record ${req.params.id} by user ${req.user?._id}`);
       res.status(HTTP_CODES.OK).json(data);
     } catch (err) {
+      logger.error(`Delete failed: ${err.message}`);
       next(err);
     }
   }
--- src/server.js
+++ src/server.js
@@ -15,7 +15,6 @@ import { errorHandler } from "./middlewares/errorHandler.js";
 dotenv.config();
 const app = express();
 
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -23,6 +22,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/api", adminRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/customers", customerRoutes);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.get("/", (req, res) => {
   logger.info("Ana sayfa GET isteği geldi");
