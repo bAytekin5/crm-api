8d837449d3b8f6e4b5d497ce3490e60f69e8ce32
Berkay Aytekin <berkay.0567@gmail.com>
2025-06-06 02:22:12 +0300

    Invoice-Model #10

--- src/routes/customer.routes.js
+++ src/routes/customer.routes.js
@@ -1,10 +1,6 @@
-// routes/CustomerRoutes.js
-
 import express from "express";
-
 import CustomerService from "../services/CustomerService.js";
 import CustomerController from "../controllers/CustomerController.js";
-
 import { protect } from "../middlewares/authMiddleware.js";
 import { authorizeRoles } from "../middlewares/rbacMiddleware.js";
 import validate from "../middlewares/validateMiddleware.js";
